efde019dbab6473b8ab13820d11468a96f67be03
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-10-20 22:06:34 +0530

    Update file and

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import find_packages,setup
 
+setup(
+    name='mcq_generator',
+    version='0.0.1',
+    author='Raso',
+    author_email='raso@gmail.com',
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    package=find_packages()
+)
